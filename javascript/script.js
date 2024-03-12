a0bc203cf1f2e95ef966ba61f64f80b99a70a0a4
Sai Mangipudi <thegeekskm101@gmail.com>
2024-03-12 13:03:05 -0500

    Added Founder Cards

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,47 @@ function scrollToSection(sectionID) {
         section.scrollIntoView({ behavior: 'smooth' });
     }
 }
+
+function founderCardClicked(founderID) {
+    const founder = document.getElementById(founderID);
+    if (founder) {
+    
+        if (founderID === 'sai')
+        {
+            // open another webpage
+            window.open('https://www.saimangipudi.dev/', '_blank');
+        }
+        else if (founderID === 'alex')
+        {
+            // open another webpage
+        }
+        else if (founderID === 'brandon')
+        {
+            // open another webpage
+        }
+    }
+}
+
+function founderMouseHover(founderID)
+{
+    const founderText = document.getElementById('display-founder-name');
+
+    if (founderID === 'sai')
+    {
+        founderText.innerHTML = "Sai Mangipudi";
+    }
+    else if (founderID === 'alex')
+    {
+        founderText.innerHTML = "Alex Patron";
+    }
+    else if (founderID === 'brandon')
+    {
+        founderText.innerHTML = "Brandon Thomas";
+    }
+}
+
+function resetFounderText()
+{
+    const founderText = document.getElementById('display-founder-name');
+    founderText.innerHTML = "OUR FOUNDERS";
+}
